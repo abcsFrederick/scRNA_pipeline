fbdc0d0213870676b5c5eea7db4b46fd32f4a115
chenv3 <43543446+chenv3@users.noreply.github.com>
2018-10-05 10:22:41 -0400

    Remove unused parameter

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 unaligned="/is2/projects/CCR-SF/scratch/illumina/Processing/DEMULTIPLEX/NextSeq/180918_NS500326_0414_AHV7KHBGX7/HV7KHBGX7/outs/fastq_path/HV7KHBGX7"
 analysis="/is2/projects/CCR-SF/scratch/illumina/Processing/ANALYSIS/DATA/xchen611/working/180918_NS500326_0414_AHV7KHBGX7/HV7KHBGX7"
 ref="mm10"
-index=""
 numcells="3000,3000"
